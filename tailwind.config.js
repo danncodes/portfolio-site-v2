62873f91302fd031f4f98a7997ca8fec4cdfc7c1
Daniel <daniel@personifyxp.com>
2021-12-08 16:45:05 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mode: 'jit',
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        backgroundColor: {
+          theme: {}
+        },
+        fontFamily: {}
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
