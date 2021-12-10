1b879131702dbfe64a1fa2f045f8982bc46b64c8
Daniel <daniel@personifyxp.com>
2021-12-10 11:31:27 +0000

    Initial Experience Block

--- components/AppExperienceBlock.vue
+++ components/AppExperienceBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col sm:flex-row h-96 w-full max-w-4xl">
+  <div class="flex flex-col sm:flex-row h-96 w-full max-w-3xl">
 
     <ul class="flex overflow-x-scroll sm:overflow-x-visible p-2 sm:flex-col sm:justify-center font-fira">
 
@@ -9,9 +9,27 @@
         </li>
 
     </ul>
-    <div class="bg-green-300 flex-grow mt-4 sm:ml-4 sm:mt-0">
-        <h1>{{ companies[index].name }}</h1>
-    </div>
+    <section class="flex-grow p-3- mt-4 sm:ml-4 sm:mt-0">
+
+        <!-- Title -->
+        <div class="flex justify-center items-center flex-col sm:flex-row">
+            <h2 class="text-xl">{{companies[index].jobTitle}}</h2>
+            <h3 class="font-fira text-secondary ml-2"><span class="mx-2">@</span>{{ companies[index].name }}</h3>
+        </div>
+
+        <!-- Date -->
+        <div class="my-6">
+            <h5 class="font-fira text-xs text-primary-200 text-center">{{companies[index].date}}</h5>
+        </div>
+
+        <!-- Job Description -->
+        <ul class="my-4 flex justify-center flex-col">
+            <li class="flex mb-2 ml-1 sm:w-10/12" v-for="description in companies[index].roleDescrption" :key="description.id">
+                 <hr class="w-2 mr-4 border-[#59e4a0] border-1 px-1">
+                 <span class="font-inter text-primary-200 text-sm pl-4">{{ description.description }}</span>
+            </li>
+        </ul>
+    </section>
 
       </div>
 </template>
@@ -21,10 +39,10 @@ export default {
     data(){
         return {
             companies: [
-                {id: 0, name: "Personify XP", jobTitle: "", date: "", roleDescrption: []},
-                {id: 1, name: "CYF", jobTitle: "", date: "", roleDescrption: []},
-                {id: 2, name: "Santander", jobTitle: "", date: "", roleDescrption: []},
-                {id: 3, name: "John Lewis", jobTitle: "", date: "", roleDescrption: []},
+                {id: 0, name: "Personify XP", jobTitle: "Junior Software Engineer", date: "March 2021 - Present", roleDescrption: [{id: 1, description: ""}]},
+                {id: 1, name: "CYF", jobTitle: "Teaching Assistant", date: "May 2021 - Present", roleDescrption: [{id: 1, description: ""}]},
+                {id: 2, name: "Santander", jobTitle: "Personal Banker", date: "August 2016 - March 2021", roleDescrption: [{id: 1, description: "I have a background working in financial services and this experience allows me to confidently communicate with stakeholders at all levels. This experience also provides me a great range of interpersonal, social and time management skills that are a core part of my working life and transferable skills which are relevant to any role. I am extremely hardworking and determined to make that transition from a personal banker to a software developer."}]},
+                {id: 3, name: "John Lewis", jobTitle: "Customer Advisor", date: "January 2014 - August 2016", roleDescrption: [{id: 1, description: ""}]},
             ],
             index: 0
         }
