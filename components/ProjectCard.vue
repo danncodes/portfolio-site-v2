af6da17dd525c18a948f2b42f62721466692c07d
Daniel <daniel@personifyxp.com>
2021-12-12 21:21:43 +0000

    Initial Project Section

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,52 +1,34 @@
 <template>
-  <main class="relative">
-    <section class="w-full h-[410px] p-6 rounded-sm absolute top-0 left-0">
-    <div class="p-1 flex flex-col justify-around h-full">
-        <div class="">
-            <h4 class="font-fira text-secondary text-sm">Featured Project</h4>
-            <h3 class="font-medium text-2xl md:text-3xl lg:text-4xl duration-200 mb-3">Movie App</h3>
-        </div>
-        <div>
-            <p class="text-sm sm:text-base"> minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.</p>
-        </div>
+  <main class="flex flex-col max-w-lg mx-auto my-8">
 
-        <div>
-            <ul class="flex my-4 gap-x-2">
-                <li class="mr-2 font-normal text-xs italic">Vue</li>
-                <li class="mr-2 font-normal text-xs italic">Nuxt</li>
-                <li class="mr-2 font-normal text-xs italic">Tailwind</li>
-                <li class="mr-2 font-normal text-xs italic">API</li>
-            </ul>
-
-            <div class="flex gap-x-2">
-                <img src="../static/github-icon.svg" alt="Github Icon" class="h-5 w-5 object-cover stroke-white">
-                <img src="../static/external-link.svg" alt="External Link" class="h-5 w-5 object-cover stroke-white">
-            </div>
+    <!-- Browser Bar -->
+    <div class="h-6 bg-[#ddd] w-full text-[#8a8a8a] flex items-center relative">
+        <!-- Browser Icons -->
+        <div class="gap-x-1 h-6 ml-5 flex justify-around items-center absolute left-0">
+            <div class="h-3 w-3 rounded-full bg-[#ff5f57]"></div>
+            <div class="h-3 w-3 rounded-full bg-[#ffbd2e]"></div>
+            <div class="h-3 w-3 rounded-full bg-[#28ca41]"></div>
+        </div>
+        
+        <!-- Browser Tab Name -->
+        <div class="w-full">
+            <h2 class="text-center text-xs font-semibold">{{project.title}}</h2>
         </div>
     </div>
+
+    <section class="project w-full">
+        <img :src="project.img" alt="" class="object-fill">
     </section>
-
-
-    <section class="test w-full h-[410px] p-6 rounded-sm absolute top-0 left-0">
-  </section>
-</main>
+  </main>
 </template>
 
 <script>
 export default {
+    props: ["project"]
 
 }
 </script>
 
 <style scoped>
 
-.test {
-    background-image: url(../static/movieApp.png);
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-color: #59e4a0;
-    background-blend-mode: multiply;
-    opacity: 0.1;
-}
 </style>
--- components/Work.vue
+++ components/Work.vue
@@ -1,9 +1,12 @@
 <template>
-  <main id="work" class="min-h-screen pt-24 p-6 md:p-12 lg:px-20 xl:px-40 relative">
+  <main id="work" class="min-h-screen pt-24 p-6 md:p-12 lg:px-20 xl:px-40 relative my-12">
     <section class="max-w-4xl mx-auto">
         <AppSectionHeader title="Projects"/>
 
-        <ProjectCard />
+      <div class="my-12 flex flex-col gap-y-3">
+        <ProjectCard v-for="project in projects" :key="project.id" :project="project"/>
+      </div>
+
     </section>
     <BackgroundCircle customClasses="h-24 sm:h-48 md:h-64 lg:h-96 left-0 bottom-0 rotate-180"/>
   </main>
@@ -11,6 +14,15 @@
 
 <script>
 export default {
+  data(){
+    return {
+      projects: [
+        {id: 1, title: "TMDB Movie App", img: require("../static/movieApp.png"), liveLink: "", githubLink: ""},
+        {id: 2, title: "Nuxt Marketing Site", img: require("../static/marketingSite.png"), liveLink: "", githubLink: ""},
+        {id: 3, title: "Journal Web Application", img: require("../static/journalApp.png"), liveLink: "", githubLink: ""}
+        ]
+    }
+  }
 
 }
 </script>
