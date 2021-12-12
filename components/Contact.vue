1bf7c28008d927c7e624607a30b314ccea08d538
Daniel <daniel@personifyxp.com>
2021-12-12 22:46:25 +0000

    Form Initial Setup

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <main id="contact" class="min-h-screen duration-200 pt-24 p-6 md:p-12 lg:px-20 xl:px-40">
+  <main id="contact" class="min-h-screen duration-200 pt-24 p-6 md:p-12 lg:px-20 xl:px-40 relative">
       <AppSectionHeader title="Contact" />
     <section class="flex flex-col w-full">
 
@@ -8,7 +8,7 @@
             <p class="text-sm text-center">Wanna get in touch or talk about a project? Feel free to contact me via email at <a class="font-fira text-secondary" href="mailto:d.mensah@outlook.com?subject=Hey I checked out your portfolio">d.mensah@outlook.com</a> or complete the contact form below</p>
         </div>
 
-        <form class="max-w-xs h-96 w-full mx-auto rounded-sm p-2">
+        <form class="max-w-xs h-96 w-full mx-auto rounded-sm p-2 text-black" @submit.prevent="formSubmitted">
             <input type="text" class="p-2 w-full my-2 rounded-sm bg-gray-50 placeholder-[#b0b1bc] outline-none duration-300 focus:ring-2 focus:ring-[#59e4a0]" placeholder="Name" required>
             <input type="email" class="p-2 w-full my-2 rounded-sm bg-gray-50 placeholder-[#b0b1bc] outline-none duration-300 focus:ring-2 focus:ring-[#59e4a0]" placeholder="Email" required>
             <textarea name="" id="contactForm" class="w-full rounded-sm p-2 bg-gray-50 placeholder-[#b0b1bc] outline-none duration-300 focus:ring-2 focus:ring-[#59e4a0]" rows="10" placeholder="Your Message ..."></textarea>
@@ -17,11 +17,18 @@
             </div>
         </form>
     </section>
+    <BackgroundRing customClasses="h-24 sm:h-48 md:h-64 lg:h-96 left-0 bottom-96 rotate-180"/>
+    <BackgroundRing customClasses="h-24 sm:h-48 md:h-64 lg:h-96 right-0 bottom-0"/>
   </main>
 </template>
 
 <script>
 export default {
+    methods: {
+        formSubmitted(){
+            console.log("Form Submitted")
+        }
+    }
 
 }
 </script>
