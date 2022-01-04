160d4484fbbba0f121548a3cee0ccba1979ad27f
Daniel <daniel@personifyxp.com>
2022-01-04 23:11:11 +0000

    Dark Mode Edits

--- components/DarkModeToggler.vue
+++ components/DarkModeToggler.vue
@@ -1,13 +1,13 @@
 <template>
-<main class="flex items-center absolute right-0 top-24 p-4 mr-6 md:mr-10 backdrop-blur-lg bg-opacity-20 bg-black cursor-pointer rounded-sm" @click="toggleDarkMode">
+<main class="flex items-center absolute right-0 stroke-current hover:stroke-[#59e4a0] top-24 p-2 md:p-3 mr-7 md:mr-10 backdrop-blur-lg bg-opacity-20 shadow hover:shadow-md duration-150 bg-black cursor-pointer rounded-sm" @click="toggleDarkMode">
 
-    <div class="cursor-pointer stroke-current hover:stroke-[#59e4a0]" v-if="!light">
+    <div class="cursor-pointer" v-if="!light">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
     </div>
 
-    <div class="cursor-pointer stroke-current hover:stroke-[#59e4a0]" v-else>
+    <div class="cursor-pointer" v-else>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
         </svg>
@@ -24,15 +24,38 @@ export default {
             light: false
         }
     },
+    mounted(){
+        this.checkLocalStorage()
+    },
     methods: {
+        checkLocalStorage(){
+            if(localStorage.getItem("modePreference")){
+                const preference = localStorage.getItem("modePreference")
+                this.setMode(preference)
+            }
+        },
         toggleDarkMode(){
             const html = document.querySelector('html')
             if(html.classList.contains('dark')){
                 html.classList.remove('dark')
-                this.light = !this.light
+                localStorage.setItem("modePreference", "light")
+                this.light = true
             } else {
                 html.classList.add('dark')
-                this.light = !this.light
+                localStorage.setItem("modePreference", "dark")
+                this.light = false
+            }
+
+        },
+        setMode(preference){
+            switch(preference){
+                case "dark":
+                    this.light = false
+                break
+                case "light":
+                    this.light = true
+                    document.querySelector('html').classList.remove("dark")
+                break
             }
 
         }
